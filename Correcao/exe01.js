973f451e6aab0af8eaa5116d10b6764d64af7a0e
PEDRO PAULO RAMOS RODRIGUES DA SILVEIRA <rodrigues.pedro@aluno.senac.rio>
2024-10-25 19:45:57 -0300

    versao01

---
+++ Correcao/exe01.js
@@ -0,0 +1,8 @@
+ 
+
+
+
+ let numeroUm = 20
+ let numeroDois = 45
+
+ console.log(numeroUm + numeroDois)
---
+++ var1.js
@@ -0,0 +1,13 @@
+//Captura a idade da pessoa
+
+let idade = 36
+
+//Armazena a idade minima para votar
+
+const idadeParaVotar = 16
+
+//Variavel que verifica se pode ou não votar
+
+let podeVotar = idade >= idadeParaVotar
+
+console.log(podeVotar)
